dab9b834962207f7fc53315ce977f086b8cebe27
IvannaMelnyk <givmiller@gmail.com>
2020-11-16 22:20:42 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2/name';
+const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 function fetchCountryByName(countryName) {
-  return fetch(`${BASE_URL}/${countryName}`).then(response => {
+  return fetch(`${BASE_URL}/name/${countryName}`).then(response => {
     return response.json();
   });
 }
